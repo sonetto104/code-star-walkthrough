c66ec24f00e62bcf827c81246ebad32de3691c3e
sonetto104 <peter_regan88@yahoo.com>
2023-06-06 10:40:18 +0000

    Set debug to false

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-sonetto104-codestarwalk-y0dkpjb2vlk.ws-eu98.gitpod.io', 'codestar-walkthrough-app.herokuapp.com', 'localhost']
 
