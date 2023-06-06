f675132735819094af521c18d2700f848be8c0d3
sonetto104 <peter_regan88@yahoo.com>
2023-06-06 10:41:20 +0000

    add x-frames setting

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['8000-sonetto104-codestarwalk-y0dkpjb2vlk.ws-eu98.gitpod.io', 'codestar-walkthrough-app.herokuapp.com', 'localhost']
 
 
